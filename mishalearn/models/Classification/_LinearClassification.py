62bdb9e4f586afc813feb31d87918500c7a932c6
Mikzarjr <mikzar.jr@gmail.com>
2025-05-09 20:55:20 +0300

    Update _LinearClassification.py

--- mishalearn/models/Classification/_LinearClassification.py
+++ mishalearn/models/Classification/_LinearClassification.py
@@ -1,7 +1,155 @@
-from typing import Optional
+from abc import ABC, abstractmethod
+from typing import Union, Optional
 
 import numpy as np
 import pandas as pd
+
+
+class BaseClassifier(ABC):
+    def __init__(self):
+        self.W: Optional[np.ndarray] = None
+        self.b: float = 0.0
+
+    @abstractmethod
+    def fit(self,
+            X: Union[pd.DataFrame, pd.Series, np.ndarray],
+            y: Union[pd.Series, np.ndarray]) -> None:
+        """
+        Model fit. Must be implemented in the child class.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def predict(self,
+                X: Union[pd.DataFrame, pd.Series, np.ndarray]) -> pd.Series:
+        """
+        Predict (+1 / -1). Must be implemented in the child class.
+        """
+        raise NotImplementedError
+
+    def _prepare_data(self, X):
+        if isinstance(X, (pd.DataFrame, pd.Series)):
+            return X.to_numpy()
+        return np.array(X)
+
+
+class SVM(BaseClassifier):
+    def __init__(self,
+                 lr: float = 0.001,
+                 lambda_param: float = 0.01,
+                 epochs: int = 1000,
+                 stochastic: bool = True):
+        super().__init__()
+        self.lr = lr
+        self.lambda_param = lambda_param
+        self.epochs = epochs
+        self.stochastic = stochastic
+
+    def fit(self,
+            X: Union[pd.DataFrame, pd.Series, np.ndarray],
+            y: Union[pd.Series, np.ndarray]) -> None:
+        X_mat = self._prepare_data(X)
+        y_vec = y.to_numpy().flatten() if isinstance(y, pd.Series) else np.array(y).flatten()
+
+        n_samples, n_features = X_mat.shape
+        self.W = np.zeros(n_features)
+        self.b = 0.0
+
+        for epoch in range(self.epochs):
+            idxs = np.random.permutation(n_samples) if self.stochastic else np.arange(n_samples)
+
+            for i in idxs:
+                x_i = X_mat[i]
+                y_i = y_vec[i]
+                condition = y_i * (np.dot(self.W, x_i) + self.b)
+
+                if condition < 1:
+                    grad_w = self.lambda_param * self.W - y_i * x_i
+                    grad_b = -y_i
+                else:
+                    grad_w = self.lambda_param * self.W
+                    grad_b = 0.0
+
+                self.W -= self.lr * grad_w
+                self.b -= self.lr * grad_b
+
+    def predict(self,
+                X: Union[pd.DataFrame, pd.Series, np.ndarray]) -> pd.Series:
+        X_mat = self._prepare_data(X)
+        scores = np.dot(X_mat, self.W) + self.b
+        preds = np.sign(scores)
+        if isinstance(X, (pd.DataFrame, pd.Series)):
+            return pd.Series(preds, index=getattr(X, 'index', None))
+        else:
+            return pd.Series(preds)
+
+
+class LinearClassification(BaseClassifier):
+    def __init__(self,
+                 max_iter: int = 1000,
+                 lr: float = 0.001,
+                 l1_alpha: Optional[float] = None,
+                 l2_alpha: Optional[float] = None,
+                 stochastic: bool = False,
+                 batch_size: Optional[float] = None):
+        super().__init__()
+        self.max_iter = max_iter
+        self.lr = lr
+        self.l1_alpha = l1_alpha
+        self.l2_alpha = l2_alpha
+        self.stochastic = stochastic
+        self.batch_size = batch_size
+
+    def _regularize(self):
+        if self.l2_alpha is not None:
+            self.W *= (1 - self.lr * self.l2_alpha)
+        if self.l1_alpha is not None:
+            self.W -= self.lr * self.l1_alpha * np.sign(self.W)
+
+    def fit(self,
+            X: Union[pd.DataFrame, pd.Series, np.ndarray],
+            y: Union[pd.Series, np.ndarray]) -> None:
+        X_mat = self._prepare_data(X)
+        n_samples, n_features = X_mat.shape
+
+        self.W = np.zeros(n_features)
+        self.b = 0.0
+
+        if self.batch_size is None:
+            bs = 1 if self.stochastic else n_samples
+        else:
+            if 0 < self.batch_size < 1:
+                bs = int(self.batch_size * n_samples)
+            else:
+                bs = int(self.batch_size)
+            bs = max(1, min(bs, n_samples))
+
+        y_vec = y.to_numpy().flatten() if isinstance(y, pd.Series) else np.array(y).flatten()
+
+        for epoch in range(self.max_iter):
+            idxs = np.random.permutation(n_samples) if self.stochastic else np.arange(n_samples)
+
+            for start in range(0, n_samples, bs):
+                batch_idx = idxs[start:start + bs]
+
+                self._regularize()
+
+                for i in batch_idx:
+                    x_i = X_mat[i]
+                    y_i = y_vec[i]
+                    margin = y_i * (np.dot(self.W, x_i) + self.b)
+                    if margin <= 0:
+                        self.W += self.lr * y_i * x_i
+                        self.b += self.lr * y_i
+
+    def predict(self,
+                X: Union[pd.DataFrame, pd.Series, np.ndarray]) -> pd.Series:
+        X_mat = self._prepare_data(X)
+        scores = X_mat.dot(self.W) + self.b
+        preds = np.sign(scores)
+        if isinstance(X, (pd.DataFrame, pd.Series)):
+            return pd.Series(preds, index=X.index)
+        return pd.Series(preds)
 
 
 class LinearClassification:
@@ -11,7 +159,7 @@ class LinearClassification:
                  l1_alpha: Optional[float] = None,
                  l2_alpha: Optional[float] = None,
                  stochastic: bool = False,
-                 batch_size: int | float = None):
+                 batch_size: int | float = 1):
 
         self._W = None
         self._max_iter = max_iter
@@ -48,10 +196,7 @@ class LinearClassification:
 
     def _stochastic_descend(self, X_mat, y_vec):
         n = X_mat.shape[0]
-        if self._batch_size is None:
-            bs = 1
-        else:
-            bs = int(self._batch_size * n) if self._batch_size < 1 else int(self._batch_size)
+        bs = int(self._batch_size * n) if self._batch_size < 1 else int(self._batch_size)
         bs = max(1, min(bs, n))
 
         for epoch in range(self._max_iter):
